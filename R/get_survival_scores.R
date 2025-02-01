ec24bcc41492e7168935930e209139456da66e56
Tiansheng Wang <40049497+tianshengwang@users.noreply.github.com>
2025-02-01 09:31:37 -0500

    Update get_survival_scores.R

--- R/get_survival_scores.R
+++ R/get_survival_scores.R
@@ -1,27 +1,5 @@
-#' Compute doubly robust survival scores for iCSF algorithm.
+#' Construct doubly robust pseudo-outcomes for E[y(T_i)] and use those in place of Yi, i.e., Y_i^* = y(Ti)/PSi if Wi=1,  Y_i^* = -y(Ti)/(1-PSi) if Wi=0. 
 #' Author: Erik Sverdrup
-#' Compute doubly robust scores for 1) survival probability estimation \eqn{P[T_i > \text{horizon}]},
-#' or 2) restricted mean survival time (RMST) \eqn{E[min(T_i, \text{horizon})]} under discrete-time right-censoring.
-#'
-#' Let \eqn{T_i} be the survival time for unit \eqn{i}. Due to censoring we only get to observe
-#' \eqn{Y_i = min(T_i, C_i)} along with an event indicator \eqn{\Delta_i = 1(T_i < C_i)}, where
-#' \eqn{C_i} is the censoring time. This function computes doubly robust scores \eqn{\hat \Gamma_i}
-#' for the quantity 1) or 2).
-#'
-#' Let \eqn{H_i = min(Y_i, \text{horizon})}. Given estimates \eqn{\hat S_T(t; X_i)} and \eqn{\hat S_C(t; X_i)}
-#' of the survival process \eqn{P[T_i > t | X_i]} and censoring process \eqn{P[C_i > t | X_i]},
-#' the scores for 1) are given by:
-#'
-#' \deqn{\hat \Gamma_i =}
-#' \deqn{\hat S_T(\text{horizon}; X_i) + }
-#' \deqn{\sum_{t=1}^{H_i - 1} \frac{1}{\hat S_C(t; X_i)}
-#'  \left(\frac{\hat S_T(\text{horizon}; X_i)}{\hat S_T(t; X_i)} - \frac{\hat S_T(\text{horizon}; X_i)}{\hat S_T(t - 1; X_i)}\right) +}
-#' \deqn{\frac{\max(\Delta_i, 1(Y_i > \text{horizon}))}{\hat S_C(H_i; X_i)}
-#'  \left(1(Y_i > \text{horizon}) -  \frac{\hat S_T(\text{horizon}; X_i)}{\hat S_T(H_i - 1; X_i)}\right).}
-#'
-#' Under regularity conditions, \eqn{\frac{1}{n} \sum_{i=1}^{n} \hat \Gamma_i} is an efficient estimate of \eqn{P[T_i > \text{horizon}]}.
-#' The scores for 2) follow a similar construction.
-#'
 #' @param Y The event time.
 #' @param D The event type \eqn{\Delta_i} (0: censored, 1: observed).
 #' @param horizon A scalar time point which defines the estimand.
@@ -62,7 +40,6 @@ get_survival_scores <- function(Y,
   # TODO len(uniq(time.grid)), horizon > min time grid, div by zero
   # need min(Y) > min(time.grid) too
 
-  # For every sample, get the time grid index corresponding to min(Yi, horizon)
   grid.index <- findInterval(pmin(Y, horizon), time.grid)
   # Get P[Ci > min(Yi, h) | Xi]
   C.Y.hat <- C.hat[cbind(seq_along(grid.index), grid.index)]
